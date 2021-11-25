2745800e11e47d6a417dc044ed6a641d02feb07f
Fahim321 <asfahim4321@gmail.com>
2021-11-25 17:13:48 +0600

    not responsive yet

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.supporters').owlCarousel({
+        loop:true,
+        margin:10,
+        items:6,
+        dots: false,
+        autoplay: true,
+    })
+
+    $('.feedback').owlCarousel({
+        loop:true,
+        margin:10,
+        items:1,
+        autoplay: true,
+    })
+});
